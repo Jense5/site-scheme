99740e169ee4f927f4e4a249c8f20b1ab5982d34
Jensen Bernard <jensenbernard5@gmail.com>
2017-02-26 01:27:36 +0100

    Add resize

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 import path from 'path';
 import fse from 'fs-extra';
 import isUrl from 'is-url';
+import sharp from 'sharp';
 import winston from 'winston';
 import capture from 'web-capture';
 import fetchScheme from 'color-extract';
@@ -10,25 +11,35 @@ import { createColorPattern } from 'color-blocks';
 
 const CACHE = path.resolve(__dirname, '5fba6621adbd97fec1d933874bf49e5d.png');
 
+const resize = input => new Promise((resolve, reject) => {
+  sharp(input).resize(100).png().toFile(CACHE, (error, info) => {
+    if (error) { reject(error); }
+    if (!error) { resolve(info); }
+  });
+});
+
 export default (url: string, output: ?string) =>
   new Promise((resolve, reject) => {
     if (isUrl(url)) {
       capture(url, CACHE).then(() => {
         winston.debug('Fetched site...');
-        fetchScheme(CACHE, 5).then((colors) => {
-          winston.debug('Fetched scheme...');
-          fse.removeSync(CACHE);
-          if (output) {
-            const aop = path.resolve(process.cwd(), output);
-            const outputPath = path.extname(aop) === '.png' ? aop : path.resolve(aop, 'output.png');
-            createColorPattern(50, colors, outputPath).then(() => {
-              resolve(colors);
-            }).catch(reject);
-          } else { resolve(colors); }
-        }).catch((error) => {
-          fse.removeSync(CACHE);
-          reject(error);
-        });
+        resize(CACHE).then(() => {
+          winston.debug('Resized site...');
+          fetchScheme(CACHE, 5).then((colors) => {
+            winston.debug('Fetched scheme...');
+            fse.removeSync(CACHE);
+            if (output) {
+              const aop = path.resolve(process.cwd(), output);
+              const outputPath = path.extname(aop) === '.png' ? aop : path.resolve(aop, 'output.png');
+              createColorPattern(50, colors, outputPath).then(() => {
+                resolve(colors);
+              }).catch(reject);
+            } else { resolve(colors); }
+          }).catch((error) => {
+            fse.removeSync(CACHE);
+            reject(error);
+          });
+        }).catch(reject);
       }).catch(reject);
     } else { reject('Invalid url provided.'); }
   });
