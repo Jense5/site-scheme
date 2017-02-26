54656159fb4506b1756a14ab78f10b819073b9cb
Jensen Bernard <jensenbernard5@gmail.com>
2017-02-26 13:20:53 +0100

    Implemented tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,32 @@
+import path from 'path';
+import fse from 'fs-extra';
 import { expect } from 'chai';
-import index from '../src/index';
+import extract from '../src/index';
 
 describe('Default tests', () => {
-  it('should add', () => { expect(index.add(2, 4)).to.equal(6); });
-  it('Should subtract', () => { expect(index.subtract(2, 4)).to.equal(-2); });
+  it('should extract colors with output', (done) => {
+    const output = path.resolve(__dirname, 'output.png');
+    extract('https://toledo.kuleuven.be', 5, output).then((colors) => {
+      expect(colors).to.be.an('array');
+      expect(colors).to.have.length(5);
+      expect(fse.existsSync(output)).to.equal(true);
+      fse.removeSync(output);
+      done();
+    }).catch(done);
+  }).timeout(25000);
+  it('should extract colors without output', (done) => {
+    const output = path.resolve(__dirname, 'output.png');
+    extract('https://toledo.kuleuven.be', 5).then((colors) => {
+      expect(colors).to.be.an('array');
+      expect(colors).to.have.length(5);
+      expect(fse.existsSync(output)).to.equal(false);
+      done();
+    }).catch(done);
+  }).timeout(25000);
+  it('should handle invalid url', (done) => {
+    extract('blabla', 5).then(() => done('error!')).catch(() => done());
+  }).timeout(25000);
+  it('should handle unexisting url', (done) => {
+    extract('http://qskjdjqsdhgkqjnflqskdnf.net', 5).then(() => done('error!')).catch(() => done());
+  }).timeout(25000);
 });
