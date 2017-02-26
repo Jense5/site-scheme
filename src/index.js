88ec7a5e54893fe4bbb54adb28662e6a1b7dcfd6
Jensen Bernard <jensenbernard5@gmail.com>
2017-02-26 01:14:00 +0100

    Add basic implementation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 // @flow
 
-export default {
-  add: (a: number, b: number) => a + b,
-  subtract: (a: number, b: number) => a - b,
-};
+import path from 'path';
+import fse from 'fs-extra';
+import isUrl from 'is-url';
+import capture from 'web-capture';
+import fetchScheme from 'color-extract';
+import { createColorPattern } from 'color-blocks';
+
+const CACHE = path.resolve(__dirname, '5fba6621adbd97fec1d933874bf49e5d.png');
+
+export default (url: string, output: ?string) =>
+  new Promise((resolve, reject) => {
+    if (isUrl(url)) {
+      capture(url, CACHE).then(() => {
+        fetchScheme(CACHE, 5).then((colors) => {
+          fse.removeSync(CACHE);
+          if (output) {
+            const aop = path.resolve(process.cwd(), output);
+            const outputPath = path.extname(aop) === '.png' ? aop : path.resolve(aop, 'output.png');
+            createColorPattern(50, colors, outputPath).then(() => {
+              resolve(colors);
+            }).catch(reject);
+          } else { resolve(colors); }
+        }).catch((error) => {
+          fse.removeSync(CACHE);
+          reject(error);
+        });
+      }).catch(reject);
+    } else { reject('Invalid url provided.'); }
+  });
